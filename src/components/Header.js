f28364e532bb0e4a1fe3cba8fc85f390aae4a54a
Vinícius Lima <vinicius43333@gmail.com>
2023-01-29 11:18:46 -0300

    Small visual updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Header(){
   return(
-    <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow-sm">
       <Link to={'/'} className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6">Company name</Link>
       <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
