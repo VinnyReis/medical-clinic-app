a716f9584300c9e449eade04f09c10ce2e7025ee
Vinícius Lima <vinicius43333@gmail.com>
2023-01-29 19:47:21 -0300

    Visual Updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,14 @@ import { Link } from 'react-router-dom';
 
 function Header(){
   return(
-    <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow-sm">
-      <Link to={'/'} className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6">Company name</Link>
+    <header className="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow-sm">
+      <Link to={'/'} className="navbar-brand col-md-3 col-lg-2 me-0 px-3 py-3 fs-6"><h5>Clinica Médica</h5></Link>
       <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <input className="form-control form-control-light w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search"/>
-      <div className="navbar-nav">
+      <div className="d-none d-md-block navbar-nav">
         <div className="nav-item text-nowrap">
-          <Link to={'/'} className="nav-link px-3">Sign out</Link>
+          <Link to={'/'} className="nav-link px-3 text-light">Sign out</Link>
         </div>
       </div>
     </header>
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,26 +2,40 @@ import { Link } from 'react-router-dom';
 
 function SideBar(){
   return(
-    <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div className="position-sticky pt-3 sidebar-sticky">
-        <ul className="nav flex-column">
-          <li className="nav-item">
-            <Link to={'/'} className="nav-link active" aria-current="page">
-              Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={'novo_agendamento'} className="nav-link">
-              Novo Agendamento
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={'agendamentos'} className="nav-link">
-              Agendamentos
-            </Link>
-          </li>
+    <div className='col-md-3 col-lg-2 d-none d-md-flex flex-column flex-shrink-0 p-3 bg-light sidebar'>
+      <ul className='nav nav-pills flex-column mb-auto'>
+        <li className='nav-item'>
+          <Link to={'/'} className='nav-link active' aria-current='page'>
+            <span data-feather='home' className='align-text-bottom'></span>
+            Dashboard
+          </Link>
+        </li>
+        <li>
+          <Link to={'novo_agendamento'} className='nav-link'>
+            <span data-feather='file' className='align-text-bottom'></span>
+            Novo Agendamento
+          </Link>
+        </li>
+        <li>
+          <Link to={'agendamentos'} className='nav-link'>
+            <span data-feather='shopping-cart' className='align-text-bottom'></span>
+            Agendamentos
+          </Link>
+        </li>
+      </ul>
+      <hr/>
+      <div className='dropdown'>
+        <a href='#' className='d-flex align-items-center link-dark text-decoration-none dropdown-toggle' data-bs-toggle='dropdown' aria-expanded='false'>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png' alt='' width='32' height='32' className='rounded-circle me-2'/>
+          <strong>Usuário</strong>
+        </a>
+        <ul className='dropdown-menu text-small shadow'>
+          <li><Link to={'/'} className='dropdown-item'>Perfil</Link></li>
+          <li><Link to={'/'} className='dropdown-item'>Configurações</Link></li>
+          <li><hr className='dropdown-divider'/></li>
+          <li><Link to={'/'} className='dropdown-item'>Sign out</Link></li>
         </ul>
       </div>
-    </nav>
+    </div>
   )
 } export default SideBar;
