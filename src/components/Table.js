0f92526403aef66fcf917fa3b0e13757c39a58b5
Vinícius Lima <vinicius43333@gmail.com>
2023-01-30 07:14:26 -0300

    Agendamentos WIP

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,24 +1,26 @@
 function Table({ columns, data }){
 
   return(
-    <table className='table table-bordered rounded overflow-hidden align-middle'>
-      <thead>
-        <tr>
-          {columns.map((column, i)=> <TableHeadItem title={column.title} key={i}/>)}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, i) => <TableRow rowData={row} columns={columns} key={i}/>)}
-      </tbody>
-    </table>
+    <div className='table-responsive'>
+      <table className='table table-bordered rounded overflow-hidden align-middle'>
+        <thead>
+          <tr>
+            {columns.map((column, i)=> <TableHeadItem title={column.title} key={i}/>)}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, i) => <TableRow rowData={row} columns={columns} key={i}/>)}
+        </tbody>
+      </table>
+    </div>
   )
 } export default Table;
 
-const TableHeadItem = ({title}) => <th className='table-light' scope='col'>{title}</th>
+const TableHeadItem = ({title}) => <th className='table-light text-nowrap' scope='col'>{title}</th>
 
 const TableRow = ({rowData, columns}) => {
   return(
-    <tr className='text-align-center'>
+    <tr className='text-align-center text-nowrap'>
       {columns.map((column, i) => {
         let cellData = rowData[`${column.dataIndex}`];
         cellData = column.render ? column.render(column.dataIndex ? cellData: rowData) : cellData;
--- src/pages/Agendamentos.js
+++ src/pages/Agendamentos.js
@@ -1,51 +1,44 @@
 import Table from '../components/Table';
+import Input from '../components/Input';
+import Button from '../components/Button';
+import { Search } from 'react-feather';
+import agendamentos from '../mock/agendamentos.json';
+import medicos from '../mock/medicos.json';
+import pacientes from '../mock/pacientes_agenda.json';
 
 function Agendamentos(){
 
-  const columns = [
-    {
-      title: 'Paciente',
-      dataIndex: 'paciente'
-    },
-    {
-      title: 'Médico',
-      dataIndex: 'medico'
-    },
-    {
-      title: 'Pagamento',
-      dataIndex: 'pagamento'
-    },
-    {
-      title: 'Status',
-      dataIndex: 'status'
-    }
-  ];
+  const pacientePorID = (id) => {
+    return pacientes.find(el => el?.id === id)?.nome;
+  }
 
-  const data = [
-    {
-      paciente: 'Vinícius Lima',
-      medico: 'João de Deus',
-      pagamento: 'Pago',
-      status: 'Atendido'
-    },
-    {
-      paciente: 'Patrícia Luciana',
-      medico: 'Jucelino pagavista',
-      pagamento: 'Pago',
-      status: 'Aguardando'
-    },
-    {
-      paciente: 'Maria José',
-      medico: 'Maria Dolores',
-      pagamento: 'Pendente',
-      status: 'Aguardando'
-    }
+  const medicoPorID = (id) => {
+    return medicos.find(el => el?.id === id)?.nome;
+  }
+
+  const columns = [
+    { title: 'Paciente', dataIndex: 'paciente', render: pacientePorID},
+    { title: 'Médico', dataIndex: 'medico', render: medicoPorID},
+    { title: 'Data', render: (agendamento) => `${agendamento.data} ${agendamento.horario}`},
+    { title: 'M. Pagamento', dataIndex: 'pagamento' },
+    { title: 'Valor', dataIndex: 'valor', render: (valor) => 'R$' + valor},
+    { title: 'Status', dataIndex: 'status' }
   ];
 
   return(
-    <Table
-      columns={columns}
-      data={data}
-    />
+    <>
+      <div className='col-12 col-lg-6'>
+        <Input
+          type='text'
+          placeholder='Busca rápida por nome do paciente'
+          grouped
+          extra={<Button type='primary' children={<Search size={22}/>}/>}
+        />
+      </div>
+      <Table
+        columns={columns}
+        data={agendamentos}
+      />
+    </>
   )
 } export default Agendamentos;
