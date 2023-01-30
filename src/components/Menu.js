ed64ba1f661b427bf923336574e98ba328574fc5
Vinícius Lima <vinicius43333@gmail.com>
2023-01-29 22:04:26 -0300

    Added menu component

---
+++ src/components/Menu.js
@@ -0,0 +1,24 @@
+import { Link, useLocation } from 'react-router-dom';
+
+function Menu(){
+
+  const location = useLocation();
+
+  const menus = [
+    {label: 'Dashboard', path: '/'},
+    {label: 'Novo Agendamento', path: '/novo_agendamento'},
+    {label: 'Agendamentos', path: '/agendamentos'}
+  ]
+
+  return(
+    <ul className='nav nav-pills flex-column mb-auto'>
+      {menus.map(menu =>
+        <li className='nav-item' key={menu.path}>
+          <Link to={menu.path} className={`nav-link ${menu.path === location.pathname ? 'active' : '' }`} aria-current='page'>
+            {menu.label}
+          </Link>
+        </li>
+      )}
+    </ul>
+  )
+} export default Menu;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,34 +1,15 @@
+import Menu from './Menu';
 import { Link } from 'react-router-dom';
 
 function SideBar(){
   return(
     <div className='col-md-3 col-lg-2 d-none d-md-flex flex-column flex-shrink-0 p-3 bg-light sidebar'>
-      <ul className='nav nav-pills flex-column mb-auto'>
-        <li className='nav-item'>
-          <Link to={'/'} className='nav-link active' aria-current='page'>
-            <span data-feather='home' className='align-text-bottom'></span>
-            Dashboard
-          </Link>
-        </li>
-        <li>
-          <Link to={'novo_agendamento'} className='nav-link'>
-            <span data-feather='file' className='align-text-bottom'></span>
-            Novo Agendamento
-          </Link>
-        </li>
-        <li>
-          <Link to={'agendamentos'} className='nav-link'>
-            <span data-feather='shopping-cart' className='align-text-bottom'></span>
-            Agendamentos
-          </Link>
-        </li>
-      </ul>
-      <hr/>
+      <Menu/>
       <div className='dropdown'>
-        <a href='#' className='d-flex align-items-center link-dark text-decoration-none dropdown-toggle' data-bs-toggle='dropdown' aria-expanded='false'>
+        <Link to={'/'} className='d-flex align-items-center link-dark text-decoration-none dropdown-toggle' data-bs-toggle='dropdown' aria-expanded='false'>
           <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png' alt='' width='32' height='32' className='rounded-circle me-2'/>
           <strong>Usuário</strong>
-        </a>
+        </Link>
         <ul className='dropdown-menu text-small shadow'>
           <li><Link to={'/'} className='dropdown-item'>Perfil</Link></li>
           <li><Link to={'/'} className='dropdown-item'>Configurações</Link></li>
